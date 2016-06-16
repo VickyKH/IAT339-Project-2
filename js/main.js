32c3932dcad32b501c2b3ce8278a9d6916c8cf4d
DoggyD <doggy883@hotmail.com>
2016-06-16 00:17:52 -0700

    More changes to pages. Added some Javascript (or at least attempted to).

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(
+	function() {
+
+    var pageWidth = $(window).width();
+    var pageHeight = 0;
+
+    console.log("Page width: " + pageWidth);
+    console.log("Page height " + pageHeight);
+
+    $("nav").removeClass("isOpen");
+    $("nav").addClass("isClosed");
+    var navOpen = 0;		// Navigation is closed
+    var headerLocked = false;	// Header unlocked
+
+    $("#link-menu").click(
+			function(clickEvent) {
+				clickEvent.preventDefault();
+
+				if (navOpen == 0) {
+					$("nav").removeClass("isClosed");
+					$("nav").addClass("isOpen");
+					navOpen = 1;
+				}
+				else {
+					$("nav").removeClass("isOpen");
+					$("nav").addClass("isClosed");
+					navOpen = 0;
+				}
+
+			}
+		);
+
+
+  }
+);
