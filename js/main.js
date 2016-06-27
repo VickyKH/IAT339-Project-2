8a87a5dfce62e4e6d19869b383521118f4c4c126
DoggyD <doggy883@hotmail.com>
2016-06-27 00:43:52 -0700

    Edited Javascript

--- js/main.js
+++ js/main.js
@@ -1,33 +1,44 @@
 $(document).ready(
 	function() {
 
-    var pageWidth = $(window).width();
-    var pageHeight = 0;
-
-    console.log("Page width: " + pageWidth);
-    console.log("Page height " + pageHeight);
-
-    var navOpen = 0;		// Navigation is closed
+    var navOpen = true;		// Navigation is open
     var headerLocked = false;	// Header unlocked
 
+    $(window).resize(function() {
+      var width = $(window).width();
+      var height = $(window).height();
+      console.log("Window width = " + width);
+      console.log("Window height = " + height);
+
+      if(width <= 555) {
+        $("nav").removeClass("isOpen");
+        $("nav").addClass("isClosed");
+        navOpen = true;
+      }
+      else {
+        $("nav").removeClass("isClosed");
+        $("nav").addClass("isOpen");
+        navOpen = false;
+      }
+
+    );
+
     $(".navlink-menu").click(
-			function(clickEvent) {
-				clickEvent.preventDefault();
+      function(clickEvent) {
+        clickEvent.preventDefault();
 
-				if (navOpen == 0) {
-					$("nav").removeClass("isClosed");
-					$("nav").addClass("isOpen");
-					navOpen = 1;
-				}
-				else {
-					$("nav").removeClass("isOpen");
-					$("nav").addClass("isClosed");
-					navOpen = 0;
-				}
-
-			}
-		);
-
+        if (navOpen == true) {
+          $("nav").removeClass("isClosed");
+          $("nav").addClass("isOpen");
+          navOpen = false;
+        }
+        else {
+          $("nav").removeClass("isOpen");
+          $("nav").addClass("isClosed");
+          navOpen = true;
+        }
+      }
+    );
 
   }
 );
