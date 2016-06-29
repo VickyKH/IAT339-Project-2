7d09b2a44aaf1a18177773179232c30ee4368103
DoggyD <doggy883@hotmail.com>
2016-06-28 23:52:43 -0700

    New updates

--- js/main.js
+++ js/main.js
@@ -1,10 +1,56 @@
 $(document).ready(
 	function() {
 
-    var navOpen = true;		// Navigation is open
-    var headerLocked = false;	// Header unlocked
+		console.log("jQuery is ready!");
+
+    var headerLocked = false;	// Header
+
+		var width = $(window).width();
+		var height = $(window).height();
+		console.log("Window width = " + width);
+		console.log("Window height = " + height);
+
+		if (width <= 766) {
+			$("nav").removeClass("navOpen");
+			$("nav").addClass("navClosed");
+			$(".navlink").hide();
+		}
+		else {
+			$("nav").removeClass("navClosed");
+			$("nav").addClass("navOpen");
+			$(".navlink").show();
+		}
 
     $(window).resize(function() {
+      var width = $(window).width();
+      var height = $(window).height();
+      console.log("Window width = " + width);
+      console.log("Window height = " + height);
+
+      if (width <= 766) {
+        $("nav").removeClass("navOpen");
+        $("nav").addClass("navClosed");
+				$(".navlink").hide();
+      }
+      else {
+        $("nav").removeClass("navClosed");
+        $("nav").addClass("navOpen");
+				$(".navlink").show();
+      }
+    });// End of if statement
+
+    $(".navlink-menu").click(
+      function(e) {
+				e.preventDefault();
+        $(".navlink").slideToggle(500);
+      }
+    )
+
+  }
+
+);
+
+/**    $(window).resize(function() {
       var width = $(window).width();
       var height = $(window).height();
       console.log("Window width = " + width);
@@ -20,8 +66,7 @@ $(document).ready(
         $("nav").addClass("isOpen");
         navOpen = false;
       }
-
-    );
+		);
 
     $(".navlink-menu").click(
       function(clickEvent) {
@@ -41,4 +86,4 @@ $(document).ready(
     );
 
   }
-);
+);**/
